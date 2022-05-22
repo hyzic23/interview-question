7045ad802088b3ea855dc0ba9afaf9dc7bfca2b5
hyzic <isaac.oghenevwapo@gmail.com>
2022-05-22 18:40:13 -0400

    Created Util class called BackBaseUtil containing both encrypt and decrypt method for the url

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.utils.BackBaseUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	BackBaseUtil utils;
+
 	@Test
 	void contextLoads() {
 	}
+
+
+	@Test
+	void HashString() throws Exception {
+		String url = "https://stash.backbase.com/projects/PO/repos/payment-order-integration-spec/browse/src/main/resources/schemas/definitions.json";
+		byte[] result = utils.encrypt(url);
+		System.out.println("#############  Encrypted URL is " + result);
+
+		String decrypted = utils.decrypt(result);
+		System.out.println("############# Decrypted URL is " + decrypted);
+	}
+
+
 
 }
